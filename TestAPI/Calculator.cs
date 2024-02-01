7c5e8aa5d203ca32a0ceeeb6e33ef0a5fc5ecbdd
SohelFaeezaSabreen <147281093+SohelFaeezaSabreen@users.noreply.github.com>
2024-02-01 00:09:00 +0000

    last commit

--- TestAPI/Calculator.cs
+++ TestAPI/Calculator.cs
@@ -13,5 +13,33 @@ namespace MyApp.Namespace
            decimal sum = number1 + number2;
             return sum;
         }
+
+        [HttpGet]
+        public decimal Subtract(decimal number1=10, decimal number2=10)
+        {  
+           decimal difference = number1 - number2;
+            return difference;
+        }
+
+        [HttpGet]
+        public decimal Multiply(decimal number1, decimal number2)
+        {  
+           decimal product = number1 * number2;
+            return product;
+        }
+
+        [HttpGet]
+        public decimal Divide(decimal number1, decimal number2)
+        {  
+           decimal quotient = number1 / number2;
+            return quotient;
+        }
+
+        [HttpGet]
+        public decimal Modulo(decimal number1, decimal number2)
+        {  
+           decimal remainder = number1 % number2;
+            return remainder;
+        }
     }
 }
